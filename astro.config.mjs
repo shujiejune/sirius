ff30baf5f89cd8fe14cfb560f712cc71ed33aaed
shujiejune <jieniu.june@tutanota.com>
2026-02-14 23:20:47 -0800

    build: update hosted URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://shujiejune.github.io",
+  base: "/sirius",
   integrations: [mdx(), sitemap()],
 
   vite: {
