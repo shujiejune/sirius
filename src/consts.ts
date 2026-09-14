f083cdc5d1b52d41c6ae854940cee7ed9042ad0f
shujiejune <jieniu.june@tutanota.com>
2026-09-13 23:20:49 -0700

    chore(index): adjust the fonts

--- src/consts.ts
+++ src/consts.ts
@@ -4,10 +4,10 @@
 export const SITE_TITLE = "Sirius";
 export const SITE_DESCRIPTION = "My blog theme";
 export const SITE_AUTHOR = "jieniu";
-export const SITE_SIGNATURE = "情在綢繆歌白紵，心同慷慨贈青萍。";
+export const SITE_SIGNATURE = "情在綢繆歌白紵 心同慷慨贈青萍";
 
 export const SOCIAL_LINKS = [
-  { name: "mastodon", url: "https://mstdn.social/@chimonanthus_praecox", label: "Mastodon" },
+  { name: "mastodon", url: "https://unstable.icu/@upwind_withe", label: "Mastodon" },
   { name: "ao3", url: "https://archiveofourown.org/users/vvithe", label: "AO3" },
   { name: "github", url: "https://github.com/shujiejune", label: "GitHub" },
 ];
